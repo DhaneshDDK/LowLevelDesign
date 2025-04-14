6a5c30621534e424bfc642dddfb2311a67b859fb
DhaneshDDK <dandedhanesh899@gmail.com>
2025-04-14 22:58:40 +0530

    code update

--- DesignPatterns/BehavioralPatterns/Momento/Main.cpp
+++ DesignPatterns/BehavioralPatterns/Momento/Main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string>
 #include "TextEditor.cpp"
 #include "History.cpp"
 
@@ -7,16 +8,32 @@ using namespace std;
 int main(){
     TextEditor editor;
     History history;
-    editor.write("Hello, ");
-    history.save(&editor);
-    cout << "Current content: " << editor.getContent() << endl;
-    editor.write("World!");
-    history.save(&editor);
-    cout << "Current content: " << editor.getContent() << endl;
-    history.undo(&editor);
-    cout << "After undo: " << editor.getContent() << endl;
-    history.undo(&editor);
-    cout << "After undo: " << editor.getContent() << endl;
-    history.undo(&editor);
+    
+    enum Action { WRITE, READ, UNDO };
+
+    while(true) {
+        cout << "Choose an action (0: Write, 1: Read, 2: Undo, 3: Exit): ";
+        int choice;
+        cin >> choice;
+
+        if (choice == 3) {
+            break;
+        } else if (choice == WRITE) {
+            string text;
+            cout << "Enter text to write: ";
+            cin.ignore(); // Clear the newline character from the input buffer
+            getline(cin, text);
+            editor.write(text);
+            history.save(&editor);
+            cout << "Text written." << editor.getContent()<< endl;
+        } else if (choice == READ) {
+            cout << "Current content: " << editor.getContent() << endl;
+        } else if (choice == UNDO) {
+            history.undo(&editor);
+            cout << "After undo, current content: " << editor.getContent() << endl;
+        } else {
+            cout << "Invalid choice. Please try again." << endl;
+        }
+    }
 
 }
